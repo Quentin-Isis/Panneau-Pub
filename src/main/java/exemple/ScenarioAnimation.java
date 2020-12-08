21631321580b5b93f8a700ea34578e7e1461e0d8
QUENTIN <QUENTIN@QJ-PC-91.home>
2020-12-08 09:11:25 +0100

    Tp bandeau

--- src/main/java/exemple/ScenarioAnimation.java
+++ src/main/java/exemple/ScenarioAnimation.java
@@ -15,26 +15,52 @@ import java.awt.Font;
  */
 public class ScenarioAnimation { 
     
-
+    public Bandeau bandeau = new Bandeau();
+    public int r = 1;
+    public int z = 2;
+    public int c = 1;
+    
     
     public static void main(String[] args){
         new ScenarioAnimation ().scenario();
     }
+   
+    public void effetRotation(){
+        
+        for (int i = 0; i<=r-1; i++) {
+           AnimRotation rotation = new AnimRotation(bandeau);
+        rotation.anim(); 
+        }
+    }
+    
+    public void effetZoom(){
+        
+        for (int i = 0; i<=z-1; i++) {
+           AnimZoom zoom = new AnimZoom(bandeau);
+        zoom.anim(); 
+        }
+    }
+    
+    public void effetColor(){
+        
+        for (int i = 0; i<=c-1; i++) {
+           AnimColor color = new AnimColor(bandeau);
+        color.anim(); 
+        }
+    }
+    
     public void scenario(){    
-        Bandeau bandeau = new Bandeau();
         
-        AnimRotation rotation = new AnimRotation(bandeau);
-        rotation.anim();
+        effetRotation();
         
         bandeau.sleep(1000);
         
-        AnimZoom zoom = new AnimZoom (bandeau);
-        zoom.anim();
+        effetColor();
         
         bandeau.sleep(1000);
-        
-        AnimColor color = new AnimColor (bandeau);
-        color.anim();
+         
+        effetZoom();
+       
     }
     
 }
